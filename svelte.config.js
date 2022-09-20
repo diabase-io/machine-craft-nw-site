d43e49464f130e398b443a28c04ccd2c21d36ccb
Taylor Dawson <taylorjdawson@gmail.com>
2022-09-20 12:36:24 -0700

    Add gh-page

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-static'
 import preprocess from 'svelte-preprocess'
+
+const dev = process.env.NODE_ENV === 'development'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,7 +12,20 @@ const config = {
   }),
 
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: '200.html',
+      precompress: false
+    }),
+    prerender: {
+      default: true
+    },
+    trailingSlash: 'always',
+    paths: {
+      base: dev ? '' : '/machine-craft-nw-site'
+    },
+    appDir: 'internal'
   }
 }
 
