ce01a577d9d708f719424937b15d64f2679bf86f
Taylor Dawson <taylorjdawson@gmail.com>
2022-10-25 13:12:36 -0700

    WIP: Add footer

--- src/lib/components/sections/shared/Footer.svelte
+++ src/lib/components/sections/shared/Footer.svelte
@@ -2,10 +2,9 @@
   import { Locon } from '$components'
 </script>
 
-<footer class="footer justify-between px-36 py-10 bg-base-200 text-base-content">
+<footer class="footer justify-between px-96 py-10 bg-base-200 text-base-content">
   <div class="flex items-center">
     <Locon class="w-28" locon="mcnwLightFull" />
-
     <div>
       <span class="footer-title text-primary opacity-100 text-base">Contact</span>
       <p>
@@ -15,7 +14,6 @@
       <p>Phone: (541) 451-4632 <br /> Email: info@machinecraftnw.com</p>
     </div>
   </div>
-
   <div>
     <span class="footer-title text-primary opacity-100 text-base">Explore</span>
     <a class="link link-hover" href={''}>Portfolio</a>
